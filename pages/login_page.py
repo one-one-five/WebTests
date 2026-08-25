673c1569b5b932b0e2714a463f6f783fe2650e55
null <s_a_elizarov@mail.ru>
2026-08-25 18:05:15 +0300

    урок_2: написал селекторы

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from pages.base_pages import BasePage
+from selenium.webdriver.common.by import By
+
+
+class LoginPageLocators:
+    ENTER_TAB = (By.XPATH, "//a[@title='Вход']")
+
+    QR_CODE_TAB = (By.XPATH, "//a[@title='QR-код']")
+    QR_CODE_IMAGE = (By.XPATH, "//img[@class='qr_code_image']")
+
+    LOGIN_FIELD = (By.XPATH, "//input[@data-test-id='login-input']")
+    PASSWORD_FIELD = (By.XPATH, "//input[@data-test-id='password-input']")
+    ENTER_BUTTON = (By.XPATH, "//button[@data-test-id='enter-action']")
+    QR_ENTER_BUTTON = (By.XPATH)
+    FORGOT = (By.XPATH, "//button[@data-test-id='forgot-password-link']")
+    REGISTRATION = (By.XPATH, "//button[@data-test-id='registration-action']")
+    VK = (By.XPATH, "//a[@title='Войти через VK ID']")
+    MAIL = (By.XPATH, "//a[@title='Войти через Почту']")
+    YANDEX = (By.XPATH, "//a[@title='Войти через Яндекс']")
+
+
+class LoginPageHelper(BasePage):
+    pass
